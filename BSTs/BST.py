24b3e804e162a0692ecb57b0c090ffdb3bd6a848
Jack Trimboli <67242835+JackTrimboli@users.noreply.github.com>
2022-07-18 15:58:42 -0400

    Added function implementations to BST class

--- BSTs/BST.py
+++ BSTs/BST.py
@@ -42,10 +42,61 @@ class BST:
         print(root.key)
         self.inorder_helper(root.right)
 
+    def preorder(self):
+        # print root first then children
+        self.preorder_helper(self.root)
+
+    def preorder_helper(self, root):
+        if root is None:
+            return
+        print(root.key)
+        self.preorder_helper(root.left)
+        self.preorder_helper(root.right)
+
+    def minimum(self):
+        n = self.root
+        while n.left is not None:
+            n = n.left
+        return n.key
+
+    def maximum(self):
+        n = self.root
+        while n.right is not None:
+            n = n.right
+        return n.key
+
+    def contain_helper(self, root, key):
+        if root is None:
+            return False
+        if root.key == key:
+            return True
+        elif root.key < key:
+            return self.contain_helper(root.right, key)
+        else:
+            return self.contain_helper(root.left, key)
+
+    def contain(self, key):
+        return self.contain_helper(self.root, key)
+
+    def contain_iterative(self, k):
+        x = self.root
+        while x is not None and x.key != k:
+            if k < x.key:
+                x = x.left
+            else:
+                x = x.right
+
+        return x is not None
+
 
 t = BST()
-
 for e in [11, 16, 15, 12, 4, 20, 60]:
     t.insert(e)
 
-t.inorder()
+print("contains 12?: ", t.contain(12))
+
+print("contains 5?: ", t.contain(5))
+
+print("contains 12?: ", t.contain_iterative(12))
+
+print("contains 5?: ", t.contain_iterative(5))
