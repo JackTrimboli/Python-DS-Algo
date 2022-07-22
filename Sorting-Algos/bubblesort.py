263bcc1f472b4f7bd2fc57477b9d16d2bbd4eebd
Jack Trimboli <67242835+JackTrimboli@users.noreply.github.com>
2022-07-22 00:19:18 -0400

    implemented bubblesort from memory

--- Sorting-Algos/bubblesort.py
+++ Sorting-Algos/bubblesort.py
@@ -15,9 +15,25 @@ def bubbleSort(a):
     bubbleSort_helper(a, len(a))
 
 
+def bubbleSortByMemory_helper(A, end):
+    if end != 0:
+        flag = False
+        for i in range(0, end - 1):
+            if A[i] > A[i + 1]:
+                flag = True
+                A[i], A[i + 1] = A[i], A[i + 1]  # "bubble up" larger elements
+        if flag:
+            bubbleSort_helper(A, end - 1)
+
+
+def bubbleSortByMemory(A):
+    bubbleSortByMemory_helper(A, len(A))
+
+
 a = [7, 3, 0, 1, 4]
-bubbleSort(a)
+bubbleSortByMemory(a)
 print(a)
+
 
 '''
 [7, 3, 0, 1, 4]
